1269c74d440b0508e2d749145cb300a9d854b784
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2024-03-23 15:57:16 +0800

    :sparkles: clean up school related data so that we can run all relation tests together

--- src/test/java/com/odde/atddv2/ApplicationSteps.java
+++ src/test/java/com/odde/atddv2/ApplicationSteps.java
@@ -2,6 +2,7 @@ package com.odde.atddv2;
 
 import com.github.leeonky.cucumber.restful.RestfulStep;
 import com.odde.atddv2.repo.OrderRepo;
+import com.odde.atddv2.repo.SchoolRepo;
 import com.odde.atddv2.repo.UserRepo;
 import io.cucumber.java.Before;
 import io.cucumber.java.zh_cn.当;
@@ -24,10 +25,14 @@ public class ApplicationSteps {
     @Autowired
     private OrderRepo orderRepo;
 
+    @Autowired
+    private SchoolRepo schoolRepo;
+
     @Before(order = 1)
     public void clearDB() {
         userRepo.deleteAll();
         orderRepo.deleteAll();
+        schoolRepo.deleteAll();
     }
 
     @Autowired
