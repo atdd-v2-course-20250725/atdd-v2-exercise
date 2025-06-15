d17f0ba8d6e1f924a7fdde1258986de954f83ce0
leeonky <leeonky@gmail.com>
2025-06-15 08:14:46 +0000

    Inline mock api step

--- src/test/java/com/odde/atddv2/ApiOrderSteps.java
+++ src/test/java/com/odde/atddv2/ApiOrderSteps.java
@@ -5,18 +5,23 @@ import com.odde.atddv2.entity.Order;
 import com.odde.atddv2.entity.OrderLine;
 import com.odde.atddv2.repo.OrderRepo;
 import io.cucumber.datatable.DataTable;
+import io.cucumber.java.Before;
 import io.cucumber.java.zh_cn.并且;
 import io.cucumber.java.zh_cn.当;
 import io.cucumber.java.zh_cn.那么;
+import lombok.SneakyThrows;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.matchers.Times;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.transaction.Transactional;
 
-public class ApiOrderSteps {
-    @Autowired
-    MockServer mockServer;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
+public class ApiOrderSteps {
     @Value("${binstd-endpoint.key}")
     private String binstdAppKey;
 
@@ -25,6 +30,15 @@ public class ApiOrderSteps {
 
     @Autowired
     private Api api;
+
+    @Autowired
+    private MockServerClient mockServerClient;
+
+    @Before(order = 0)
+    @SneakyThrows
+    public void resetMockServer() {
+        mockServerClient.reset();
+    }
 
     @当("API查询订单时")
     public void api查询订单时() {
@@ -47,8 +61,13 @@ public class ApiOrderSteps {
 
     @并且("存在快递单{string}的物流信息如下")
     public void 存在快递单的物流信息如下(String deliverNo, String json) {
-        mockServer.getJson("/express/query", (request) -> request.withQueryStringParameter("appkey", binstdAppKey)
-                .withQueryStringParameter("type", "auto")
-                .withQueryStringParameter("number", deliverNo), json);
+        mockServerClient.when(request()
+                        .withMethod("GET").withPath("/express/query")
+                        .withQueryStringParameter("appkey", binstdAppKey)
+                        .withQueryStringParameter("type", "auto")
+                        .withQueryStringParameter("number", deliverNo), Times.unlimited())
+                .respond(response().withStatusCode(200)
+                        .withHeader(CONTENT_TYPE, "application/json")
+                        .withBody(json));
     }
 }
